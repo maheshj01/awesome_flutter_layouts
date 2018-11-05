6bba79f2a0c351121cde016acd081307cf7a827f
maheshmnj <maheshmn121@gmail.com>
2018-11-05 15:10:21 +0530

    First Commit from Command Line

---
+++ app/src/main/java/com/maheshmnj/layouts/awesomeandroidlayouts/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.maheshmnj.layouts.awesomeandroidlayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
