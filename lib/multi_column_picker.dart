53ce5cc2a999614a794abc849cdb91f3ae1f4c37
maheshjamdade <mahesh.jamdade@codemonk.in>
2020-10-10 19:49:14 +0530

    remove debug banner

--- lib/multi_column_picker.dart
+++ lib/multi_column_picker.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: beachRed,
       ),
